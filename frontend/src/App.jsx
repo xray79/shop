11ae3f240c36b8a525a4e806f7543493d735bfb6
Ibnul Huq <ibnul.huq@gmail.com>
2023-02-10 10:41:23 +0000

    make api call and display products with getAll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home.component";
 function App() {
   return (
     <div className="App">
+      hello
       <Home />
     </div>
   );
--- frontend/src/components/Home/Home.component.jsx
+++ frontend/src/components/Home/Home.component.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+import Product from "../Product/Product.component";
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return <div>home</div>;
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/getAll")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  const handleClick = () => {
+    console.log("what");
+  };
+
+  return (
+    <>
+      Home
+      <div className={styles.productsContainer}>
+        {data ? (
+          data.map((item) => (
+            <Product
+              key={item._id}
+              item={item}
+              onClick={() => {
+                alert("he");
+              }}
+            />
+          ))
+        ) : (
+          <div className="">Loading...</div>
+        )}
+      </div>
+    </>
+  );
 };
 export default Home;
