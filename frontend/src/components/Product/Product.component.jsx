3820356b6c8a0d9194bc9dca33c184f5fe6728db
Ibnul Huq <ibnul.huq@gmail.com>
2023-02-10 10:42:01 +0000

    add component and styles for individual products

---
+++ frontend/src/components/Product/Product.component.jsx
@@ -0,0 +1,37 @@
+import styles from "./Product.module.css";
+
+const Product = ({ item }) => {
+  /**
+   * item shape:
+   * {id: num,
+   * title: string
+   * price: num
+   * description: string
+   * category: string
+   * image: string
+   * rating: obj}
+   */
+  // for (const key in item) {
+  //   console.log(`${key}: ${item[key]}`);
+  // }
+
+  return (
+    <div className={styles.product}>
+      <div className={styles.imgContainer}>
+        <img
+          className={styles.productImage}
+          src={item.image}
+          alt={item.title}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h2>{item.title}</h2>
+        <span>{item.price}</span>
+        <p>{item.description}</p>
+        <span>{item.category}</span>
+        {/* <span>{item.rating.</span> */}
+      </div>
+    </div>
+  );
+};
+export default Product;
