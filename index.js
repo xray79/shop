aa2abb710554982e99a1aeaa79190dc1ee20fa1b
Ibnul Huq <ibnul.huq@gmail.com>
2023-02-10 15:46:21 +0000

    move entry point to main folder

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const db = require("./models/db");
+const db = require("./backend/models/db");
 
 const PORT = process.env.SERVER_PORT;
 
 const app = Express();
 app.use(cors());
 
-const api = require("./routes/apiRoutes");
+const api = require("./backend/routes/apiRoutes");
 app.use("/api", api);
 
 app.listen(PORT, () => {
