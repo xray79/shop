df186558e9cb1c52a249053b65d57f4f5805f570
Ibnul Huq <ibnul.huq@gmail.com>
2023-02-24 17:28:50 +0000

    reset cart state on success

--- frontend/src/components/Success/Success.component.jsx
+++ frontend/src/components/Success/Success.component.jsx
@@ -1,4 +1,12 @@
+import { useContext, useEffect } from "react";
+import { CartContext } from "../../contexts/CartContext";
+
 const Success = () => {
+  const { setCartItems } = useContext(CartContext);
+  useEffect(() => {
+    setCartItems([]);
+  }, []);
+
   return <div>Success</div>;
 };
 export default Success;
