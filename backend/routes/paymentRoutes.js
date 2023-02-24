62129ad544144e06acb7e20b41e0a3d1ce2993ec
Ibnul Huq <ibnul.huq@gmail.com>
2023-02-24 17:29:07 +0000

    protect payment route

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
+const { expressjwt: expressJwt } = require("express-jwt");
 
 const { controlPay } = require("../controllers/paymentController");
 
-// Login and signup routes
-router.post("/pay", controlPay);
+router.post(
+  "/pay",
+  expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"] }),
+  controlPay
+);
 
 module.exports = router;
