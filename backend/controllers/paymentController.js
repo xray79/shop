168e90496898d2c0d15cc08b342ebea79e97929a
Ibnul Huq <ibnul.huq@gmail.com>
2023-02-24 17:31:20 +0000

    calculate total

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 
 // controllers for product crud operations
 const controlPay = async (req, res) => {
+  let total = 0;
+  req.body.forEach((i) => {
+    total += i.quantity * i.price;
+  });
+  console.log("Total: ", total);
   res.json({ message: "working" });
 };
 
